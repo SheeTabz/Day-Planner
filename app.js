b11c2280fd92da27c11e451b0de36b79fac8a729
SherroTabz <wmuriithitabitha@gmail.com>
2022-06-05 20:59:15 +0300

    Design changes

--- app.js
+++ app.js
@@ -1,35 +1,38 @@
 'Use Strict'
-const todoInput = document.querySelector(".todo-input")
-const todoButton = document.querySelector(".todo-Button")
-const todoList = document.querySelector(".do-list")
-    // eventLsteners
-todoButton.addEventListener('click', addActivity)
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector(".todo-Button");
+const todoList = document.querySelector(".do-list");
+// eventLsteners
+todoButton.addEventListener('click', addActivity);
 
 //functions
 function addActivity(event) {
-    //prevents the form from submitting
-    event.preventDefaut();
+    //prevents the form from 
+    event.preventDefault();
+
+
     //the ToDo list
     //creates adiv element with the class to-do
-    const todoDiv = document.createElement("div")
-    todoDiv.classList.add('todo')
-        //Create a list
-    const newtodoLi = document.createElement("li")
-    newtodoLi.innerText = 'hello'
-    newtodoLi.classList.add("todo-Item")
-        //make the list a child to the Div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    //Create a list
+    const newtodoLi = document.createElement("li");
+    newtodoLi.innerText = "hello";
+    newtodoLi.classList.add("todo-Item");
+    //make the list a child to the Div
     todoDiv.appendChild(newtodoLi);
     //add check mark button
-    const checkedButton = document.createElement('button')
-    checkedButton.classList.add('check-btn')
-    checkedButton.innerHTML = '< i class = "fas fa-check" ></i >';
+    const checkedButton = document.createElement("button");
+    checkedButton.innerHTML = '<i class = "fas fa-check" ></i >';
+    checkedButton.classList.add("check-btn");
+
     todoDiv.appendChild(checkedButton);
     //add a trash button
-    const trashButton = document.createElement('button')
-    trashButton.classList.add('trash-btn')
-    trashButton.innerHTML = '< i class = "fas fa-trash" > </i >'
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class = "fas fa-trash" ></i >'
+    trashButton.classList.add("trash-btn");
+
     todoDiv.appendChild(trashButton);
     //Append To the unordered List in the HTML 
     todoList.appendChild(todoDiv)
-
 }
